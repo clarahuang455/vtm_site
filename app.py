e957738f8eb3ca9566ee2432a5cf157bc9b04efc
Clara Huang <lijuan-huang@uiowa.edu>
2022-04-04 19:31:19 -0500

    modify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
@@ -11,8 +11,21 @@ def index():
 def about():
     return render_template('about.html',pageTitle="About")
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET', 'POST'])
 def estimate():
+    if request.method == 'POST': 
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi = 3.14
+        top = pi * radius * radius
+        sides = 2 * pi * (radius * height)
+        area = top + sides
+        square = area/144
+        material_cost = square * 25
+        labor_cost = square * 15
+        estimate_total = material_cost + labor_cost
+        return render_template('estimate.html', calculate=estimate_total)
     return render_template('estimate.html', pageTitle="Estimate")
 
 if __name__ == '__main__':
